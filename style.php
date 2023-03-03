2056463c675a83e6d0c88a7c01715078db9bb883
Mª Ángeles Romero <48793105+mariarm88@users.noreply.github.com>
2023-03-03 16:40:25 +0100

    Update style.php

--- style.php
+++ style.php
@@ -1,378 +1,205 @@
 <style>
-  /* --------------------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------------------------
-   ESTILOS MAURI
------------------------------------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------------------------------- */
-  .loader_ajax {
-    position: fixed;
-    left: 0px;
-    top: 0px;
-    width: 100%;
-    height: 100%;
-    z-index: 9998;
-    background: url('<?= base_url() ?>assets/images/loader_ajax.gif') 50% 50% no-repeat rgba(0, 0, 0, 0.50);
-  }
-
-  p {
-    font-size: 16px;
-  }
-
-  h1 {
-    font-size: 18px;
-    font-weight: bold;
-    margin: 5px !important;
-  }
-
-  .alert-danger {
-    background-color: #f2dede !important;
-    border-color: #e0b1b8 !important;
-    color: #b94a48 !important;
-  }
-
-  #imagen_cargada {
-    margin: 3vh;
-  }
-
-  .file__droparea {
-    position: relative;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    width: 100%;
-    margin-top: 10px;
-    padding: 30px 4px 4px 4px;
-    border: 2px dashed #CCC;
-    border-radius: 3px;
-    transition: 0.2s;
-    border-radius: 10px;
-  }
-
-  .file__droparea:hover {
-    border: 2px dashed #2196f3;
-  }
-
-
-  .file__droparea-icon {
-    font-size: 40px;
-    color: #CCC;
-  }
-
-  .file__droparea-btn {
-    border-radius: 8px;
-    text-transform: uppercase;
-    width: 100%;
-  }
-
-  .file__droparea-msg {
-    font-size: 20px;
-    font-weight: 300;
-    line-height: 1.4;
-    text-transform: uppercase;
-    white-space: nowrap;
-    overflow: hidden;
-    color: #CCC;
-    margin: 5px 0;
-  }
-
-  .file__droparea-input {
-    position: absolute;
-    left: 0;
-    top: 0;
-    height: 100%;
-    width: 100%;
-    cursor: pointer;
-    opacity: 0;
-  }
-
-  .file__droparea-input:hover {
-    outline: none;
-  }
-
-  .file__droparea-fileText {
-    display: none;
-    position: absolute;
-    top: 4px;
-    right: 4px;
-    background: #DDD;
-    border-radius: 50px;
-    padding: 2px 10px;
-  }
-
-  #wrap_informes {
-    min-height: 85vh;
-    background-color: #fff;
-    border: 1px solid #ccc;
-  }
-
-  /* DESARROLLOS ACTUALES */
-  #desarrollos_actuales {
-    display: block;
-    padding: 10px;
-    border: 1px solid #cccccc;
-    background-color: #fafafa;
-    min-height: 64vh;
-
-  }
-
-  .texto_anterior {
-    font-size: 14px;
-    font-weight: bold;
-  }
-
-  .estado_anterior {
-    font-size: 12px;
-    font-weight: bold;
-    overflow-y: scroll;
-  }
-
-  .herramienta {
-    font-size: 15px;
-    font-family: Arial Narrow;
-    font-weight: bold;
-
-  }
-
-
-  .subherramienta {
-    font-size: 10px;
-  }
-
-  .prioritario {
-    font-size: 18px;
-    float: right;
-    color: red;
-  }
-
-  .normal {
-    font-size: 18px;
-    float: right;
-    color: #CCC;
-  }
-
-  .comentar_herramienta {
-    font-size: 25px;
-    float: right;
-    margin-top: 7px;
-    color: orange;
-    border: hidden;
-    background: none;
-
-  }
-
-  .pendiente_herramienta {
-    font-size: 20px;
-    float: right;
-    color: orange;
-    align-self: top;
-  }
-
-  .finalizar_herramienta {
-    font-size: 20px;
-    float: right;
-    color: red;
-    align-self: top;
-  }
-
-  .informe {
-    padding: 2vh !important;
-    margin-left: 30px;
-    margin-right: 30px;
-  }
-
-  .header_nota {
-    background-color: #CCC;
-    border-radius: 5px;
-
-  }
-
-  .cuerpo_nota {
-    font-size: 12px;
-
-  }
-
-  .enlace {
-    font-weight: bold;
-  }
-
-  .imagen_manual {
-    width: 35px;
-    margin-left: 20px;
-    border-radius: 10px 10px;
-    margin-top: 1px;
-    float: right;
-  }
-
-  #desarrollos_actuales_notas {
-    min-height: 62vh;
-    resize: none;
-    font-size: 14px;
-    font-family: Arial;
-    overflow-y: scroll;
-
-  }
-
-
-  #desarrollos_actuales_en_cola {
-    min-height: 51vh;
-  }
-
-  #img {
-    width: 100%;
-  }
-
-  #herramienta_notas {
-    min-height: 25vh;
-    resize: none;
-  }
-
-  .guardar_nota{
-    float: right;
-   
-  }
-
-  /* MODAL PARTE TRABAJO */
-  #tabla_tareas>tbody>tr>td {
-    vertical-align: middle;
-    padding: 0px 0px 0px 8px;
-    border-top: none;
-    border-bottom: 1px solid #ddd;
-  }
-
-  .clase_annadir_trabajo {
-    float: right;
-    background-color: transparent;
-    border-color: transparent;
-    color: darkgreen;
-  }
-
-  .clase_annadir_trabajo:hover,
-  .clase_annadir_trabajo:focus {
-    float: right;
-    background-color: transparent;
-    border-color: transparent;
-    color: green;
-  }
-
-  .clase_borrar_tarea,
-  .clase_borrar_trabajo {
-    float: right;
-    background-color: transparent;
-    border-color: transparent;
-    color: #dd0202;
-  }
-
-  .clase_borrar_tarea:hover,
-  .clase_borrar_trabajo:hover {
-    float: right;
-    background-color: transparent;
-    border-color: transparent;
-    color: #ff0000;
-  }
-
-  .clase_borrar_tarea:focus,
-  .clase_borrar_trabajo:focus {
-    float: right;
-    background-color: transparent;
-    border-color: transparent;
-    color: #ff0000;
-  }
-
-  #cruz_cerrar {
-    color: #a21212;
-    cursor: pointer;
-    font-size: 20px;
-
-  }
-
-
-
-  /* NUEVA HERRAMIENTA */
-  #nueva_herramienta {
-    display: none;
-    padding: 10px;
-    border: 1px solid #cccccc;
-    background-color: #fafafa;
-    min-height: 64vh;
-  }
-  #nueva_herramienta_notas {
-    min-height: 58.5vh;
-    resize: none;
-  }
-
-  /* MODFICAR HERRAMIENTA */
-  #modificar_herramienta {
-    display: none;
-    padding: 11px;
-    border: 1px solid #cccccc;
-    background-color: #fafafa;
-    min-height: 64vh;
-
-  }
-
-  #modificar_herramienta_notas {
-    min-height: 58.2vh;
-    resize: none;
-
-
-  }
-
-  /* VES ESTADO DESARROLLO */
-  #ver_estado_desarrollo {
-    display: none;
-    padding: 12px;
-    border: 1px solid #cccccc;
-    background-color: #fafafa;
-    min-height: 64vh;
-  }
-
-  #ver_estado_desarrollo_notas_nuevas {
-    min-height: 19vh;
-    resize: none;
-  }
-
-  #ver_estado_desarrollo_notas_anteriores {
-    min-height: 58vh;
-    resize: none;
-    overflow-y: scroll;
-  }
-
-  #ver_historico_solicitado {
-    font-size: 11px;
-    font-weight: bold;
-    overflow-y: scroll;
-    min-height: 24vh;
-    resize: none;
-
-  }
-
-  .boton_nota{
-   float: right;
-  }
-
-
-
-  /* NOTIFICAR ERROR */
-  #notificar_error {
-    display: none;
-    padding: 10px;
-    border: 1px solid #cccccc;
-    background-color: #fafafa;
-    min-height: 64vh;
-  }
-
-  #agregar_nota_error {
-    min-height: 26.4vh;
-    resize: none;
-  }
-
-    /* DESARROLLOS INACTIVOS */
-    #desarrollos_inactivos{
-    display: block;
-    padding: 10px;
-    border: 1px solid #cccccc;
-    background-color: #fafafa;
-    min-height: 64vh;
-
-  }
+	/* --------------------------------------------------------------------------------------------------------------
+	-----------------------------------------------------------------------------------------------------------------
+	    ESTILOS MAURI
+	-----------------------------------------------------------------------------------------------------------------
+	-------------------------------------------------------------------------------------------------------------- */
+	.loader_ajax {
+		position: fixed;
+		left: 0px;
+		top: 0px;
+		width: 100%;
+		height: 100%;
+		z-index: 9998;
+		background: url('<?= base_url() ?>assets/images/loader_ajax.gif') 50% 50% no-repeat rgba(0, 0, 0, 0.50);
+	}
+
+
+	#form_alta,
+	#form_datos,
+	#modal_iyv {
+		background: url('http://10.228.69.100/viki_dev/paginas/blablaK/img/mapa_boll.png');
+		border-radius: 15px;
+		padding: 15px;
+		box-shadow: 0px 3px 2px 2px;
+		min-height: 80vh;
+		border: 1px solid #00000038;
+		background-color: #eff6ff;
+	}
+
+	/* IMAGEN PORTADA */
+	.portada {
+		margin-bottom: 3em;
+		border: 3px solid;
+		border-radius: 15px;
+		padding: 0px;
+	}
+
+	.portada img {
+		width: 100%;
+		border-radius: 10px;
+	}
+
+	/* FORMULARIOS */
+	#wrap_datos_personales,
+	#wrap_datos_trayecto,
+	#wrap_datos_dias_semana,
+	#wrap_origen_y_destino,
+	#wrap_datos_t1_horario,
+	#wrap_datos_t2_horario,
+	#wrap_datos_puntos_de_encuentro,
+	#wrap_datos_origen_y_destino,
+	#wrap_compatibles_t1,
+	#wrap_compatibles_t2,
+	#wrap_entrada_modal,
+	#wrap_salida_modal,
+	#wrap_origen_y_destino_modal,
+	#wrap_dias_semana_modal {
+		margin-bottom: 3em;
+		border: 2px solid;
+		border-radius: 15px;
+		padding: 15px;
+		background-color: #deeeff;
+		box-shadow: 0px 3px 2px 2px;
+	}
+
+	/* ESTILO DATATABLE */
+	table.dataTable {
+		text-align-last: center;
+	}
+
+	.busqueda_datatable {
+		text-align: center !important;
+		font-family: FontAwesome, 'Gill Sans', Calibri, sans-serif;
+	}
+
+	.dataTables_wrapper {
+		margin-top: -10px;
+	}
+
+	/* ANIMACION ANCHURA COL-MD */
+	#puntos_de_encuentro [class*='col-md-'] {
+		transition: all 0.5s ease-in-out !important;
+	}
+
+	/* PUNTOS DE ENCUENTRO */
+	#puntos_de_encuentro,
+	#puntos_de_encuentro_modal {
+		padding-left: 0px !important;
+		padding-right: 0px !important;
+	}
+
+
+
+	/* CHECKS DIAS SEMANA */
+	.weekDays-selector {
+		padding-left: 2rem;
+		padding-right: 2rem;
+	}
+
+	.weekDays-selector input {
+		display: none !important;
+	}
+
+	.weekDays-selector input[type=checkbox]+label {
+		display: inline-block;
+		border-radius: 6px;
+		background: #dddddd;
+		height: 3.5rem;
+		width: 20rem;
+		margin-right: 1rem;
+		line-height: 40px;
+		text-align: center;
+		cursor: pointer;
+	}
+
+	.weekDays-selector input[type=checkbox]:checked+label {
+		background: #337ab7;
+		color: #eeeeee;
+	}
+
+
+
+
+	/* TITULO DEL MODAL */
+	#titulo {
+		font-size: 25px;
+	}
+
+
+	/* CHECKS DIAS SEMANA MODAL */
+	.weekDays-selector_modal {
+		padding-left: 2rem;
+		padding-right: 2rem;
+	}
+
+	.weekDays-selector_modal input {
+		display: none !important;
+	}
+
+	.weekDays-selector_modal input[type=checkbox]+label {
+		display: inline-block;
+		border-radius: 6px;
+		background: #dddddd;
+		height: 3.5rem;
+		width: 20rem;
+		margin-right: 1rem;
+		line-height: 40px;
+		text-align: center;
+		cursor: pointer;
+	}
+
+	.weekDays-selector_modal input[type=checkbox]:checked+label {
+		background: #337ab7;
+		color: #eeeeee;
+	}
+
+
+
+	/*BOTON GUARDAR DATOS*/
+	#wrap_guardar_datos {
+		display: block;
+		margin-right: 14px;
+		float: right;
+		font-size: 15px;
+
+	}
+
+	#wrap_botonera_1,
+	#wrap_botonera_2,
+	#wrap_botonera_3,
+	#wrap_botonera_4,
+	#wrap_botonera_5,
+	#wrap_botonera_1_modal,
+	#wrap_botonera_2_modal,
+	#wrap_botonera_3_modal,
+	#wrap_botonera_4_modal,
+	#wrap_botonera_5_modal {
+		font-size: 18px;
+		float: left;
+		margin-top: 15px;
+		border: hidden;
+		background: none;
+	}
+
+
+	.clase_annadir_punto {
+		color: green;
+	}
+
+	.clase_annadir_punto_modal {
+		color: green;
+	}
+
+	.clase_borrar_punto {
+		color: red;
+	}
+
+	.clase_borrar_punto_modal {
+		color: red;
+	}
+	.menos_letra{
+		font-size: 10px;
+
+	}
 </style>
